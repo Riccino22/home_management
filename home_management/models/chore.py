9f12b19538e8ed6982048a40dd3a4806c2624b66
Riccino <riccardorinojosam@gmail.com>
2024-05-27 20:20:43 -0300

    "Added fields to Chore model: title, description, partner_id, and status"

--- home_management/models/chore.py
+++ home_management/models/chore.py
@@ -3,5 +3,16 @@ from odoo import fields, api, models
 class Chore(models.Model):
     _name = 'home.chore'
     
+    title = fields.Char("Title")
+    description = fields.Text("Description")
     executue_date = fields.Datetime("Execute date")
+    partner_id = fields.Many2one("res.partner", string="Assigned")
+    status = fields.Selection([
+        ("in_progress", "In progress"),
+        ('draft', 'Draft'),
+        ('done', 'Done'),
+        ("blocked", "Blocked"),
+        ('cancel', 'Cancel'),
+        
+    ])
     
