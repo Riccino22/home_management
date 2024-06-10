4812060a3a4ac7b6fb3514389eb718921487210d
riccino <riccardorinojosam@gmail.com>
2024-06-10 12:00:42 -0300

    created the pivot and calendar view

--- home_management/models/chore.py
+++ home_management/models/chore.py
@@ -1,11 +1,12 @@
 from odoo import fields, api, models
+from odoo.exceptions import ValidationError
 
 class Chore(models.Model):
     _name = 'home.chore'
     
     title = fields.Char("Title")
     description = fields.Text("Description")
-    executue_date = fields.Datetime("Execute date")
+    execute_date = fields.Datetime("Execute date")
     partner_id = fields.Many2one("res.partner", string="Assigned")
     status = fields.Selection([
         ("in_progress", "In progress"),
@@ -15,4 +16,15 @@ class Chore(models.Model):
         ('cancel', 'Cancel'),
         
     ])
+
+    @api.constrains("execute_date")
+    def _check_date(self):
+        for record in self:
+            if record.execute_date < fields.Datetime.now():
+                raise ValidationError("The date must be today or later")
+            
+    @api.depends("title")
+    def _compute_display_name(self):
+        for record in self:
+            record.display_name = record.title
     
